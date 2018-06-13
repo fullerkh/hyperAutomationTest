2729048571f5b8ac44c3ae8fef5c68b5aff49426
fullerkh <fullerkh@miamioh.edu>
2018-06-13 14:55:50 -0400

    last add before getting rid of geojson. IS WORKING

--- src/jsonTransformFormatting.py
+++ src/jsonTransformFormatting.py
@@ -1,13 +1,15 @@
 import json
+import re
+from geomet import wkt
 
 
 jsonsGeo = ["CINC_POLICE_NEIGHBORHOODSformatted", "CC_polygonsformatted", "SNA_polygonsformatted"]
 jsonsProperties = ["CINC_POLICE_NEIGHBORHOODSformattedOriginal", "CC_polygonsformattedOriginal", "SNA_polygonsformattedOriginal"]
 
 jsonsGeoFINAL = ["CINC_POLICE_NEIGHBORHOODSformattedFINAL", "CC_polygonsformattedFINAL", "SNA_polygonsformattedFINAL"] 
-jsonsGeoFINAL = ["convertformatted"]
+#jsonsGeoFINAL = ["convertformatted"]
 
-def grabGeoData(jsonF):
+def grabData(jsonF):
     try:
         mapProperties = []
         print "Grabbing properties data from " + jsonF + ".... \n     Please Wait"
@@ -42,6 +44,26 @@ def changeGeoJsonProperties(jsonsProperties,jsonsGeo):
 			file.write(json.dumps(data, separators  = (", ", ": "), indent = 4))
 			file.close()
 		count += 1 
+                
+def longestStr(jsonF):
+    jsonF = jsonF + ".json"
+    try:
+        counter = 0
+        with open(jsonF) as json_data:
+            data = json.load(json_data)
+            print "data opened"
+            for feature in data['features']:
+                #print feature
+                if 'geometry' in feature: 
+                    print str(feature['geometry']['coordinates'])
+                    length = len(str(feature['geometry']['coordinates']))
+                    if length > counter:
+                        print "no"
+                        counter = length
+        return counter
+    except Exception, e:
+        print "A fatal error occurred while grabbing the Geographic data in " + jsonF + ":\n", e, "\nExiting now."
+        exit(-1)
 	
 	
 # NEVER WORKED 
@@ -70,8 +92,59 @@ def changeGeoJsonProperties(jsonsProperties,jsonsGeo):
 	# return newPolygon
 		
 
-changeGeoJsonProperties(jsonsProperties,jsonsGeo)
+#changeGeoJsonProperties(jsonsProperties,jsonsGeo)
 
+def grabGeoData(jsonF):
+    try:
+        counter = 0
+        neighborhoods =[]
+        print "Grabbing Geographic Data from " + jsonF + ".... \n     Please Wait"
+        # opens json file and return the data
+        with open(jsonF) as json_data:
+            data = json.load(json_data)
+        count = 0
+        for feature in data['features']:
+            entry = {}
+            for propertyK, propertyV in feature.iteritems():
+                if propertyK == "geometry": 
+                    propertyV= geojson2wkt(propertyV)
+                    if len(propertyV) > counter:
+                        count = len(propertyV)
+                    entry.update({propertyK:propertyV})
+                    print "geo"
+                if propertyK == "properties": 
+                    for key, value in propertyV.iteritems():
+                        print key + "     " + str(value) 
+                        entry.update({key:value})
+                neighborhoods.append(entry)
+        map = {"neighborhoods" : neighborhoods}
+        print "Geographic Data has been successfully grabbed \nThe longest string was :  " +str(count)+ " chars long for this file"
+        return map
+    except Exception, e:
+        print "A fatal error occurred while grabbing the Geographic data in " + jsonF + ":\n", e, "\nExiting now."
+        exit(-1)
+      
+#------------------------------------------------------------------------------
+#   geojson2wkt
+#------------------------------------------------------------------------------
+
+def geojson2wkt(geojson):
+    #print geojson
+    coordinates = wkt.dumps(geojson, 6)
+    # print what kind of geometry we are dealing with
+    #print coordinates[0:coordinates.find(" ")]
+    return coordinates
+
+def writeJson(name, data):
+        final = json.dumps(data, separators  = (", ", ": "), indent = 4)
+	file = open(name + "-brandon1.json","w")
+	file.write(final)
+
+for map in jsonsGeoFINAL:
+    map = map + ".json"
+    data = grabGeoData(map)
+    writeJson(map, data)
+    
 
 
 ##   py -2.7 hyperAutomationtest.py -b -j shootingstestComplete.json -e shootingsWorkbook.hyper
